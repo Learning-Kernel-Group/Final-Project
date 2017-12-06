2e86dac4274290483c8e3153c57333c5a4539d6c
luca-venturi <luca.venturi.92@gmail.com>
2017-12-06 18:22:15 -0500

    new algorithms

--- pgd_l2.py
+++ pgd_l2.py
@@ -1,26 +1,27 @@
 # Luca
 
-# my_int = Algorithm 3
+# inter = Algorithm 2
 # Input: 	- features: vector of sample features
 #			- labels: vector of sample labels
-#			- degree: degree of polynomial combination
 #			- lam: KRR parameter
 #			- eta: interpolation parameter
-#			- beta: optimization problem parameter
+#			- L: optimization problem parameter (Lambda)
+#			- mu0: optimization problem parameter
 #			- eps: tolerance stopping parameter
 #			- mu_init: inital value of mu in the iteration
 #			- sumbsampling: sumbsampling factor
 # Output: 	- mu_prime: found value of mu
 #			- poly_ker: final kernel
 
-def my_int(features, labels, degree=1, lam=10., eta=0.5, beta=1., eps=1e-3, mu_init=None, subsampling=1):
-	beta = 0.5 / beta
-    (m, p) = features.shape
+def inter(features, labels, degree=1, lam=10., eta=0.5, L=1., mu0=None, eps=1e-3, mu_init=None, subsampling=1):
+	(m, p) = features.shape
     mu = mu_init
 	if mu == None:
-		mu = np.zeros(p)    
+		mu = np.zeros(p)
+	if mu0 == None:
+		mu0 = np.zeros(p)    
 	print('Start running my_int on kernels...')
-	gram = sum_weight_ker(base_kernels, mu) ** degree + lam * np.eye(m) # gram = K_mu + lam * I
+	gram = sum_weight_ker(base_kernels, mu) + lam * np.eye(m) # gram = K_mu + lam * I
 	print('Solve for alpha...')
 	al = np.linalg.solve(gram, labels)
 	al_prime = np.zeros(m)
@@ -28,20 +29,20 @@ def my_int(features, labels, degree=1, lam=10., eta=0.5, beta=1., eps=1e-3, mu_i
     while np.linalg.norm(al - al_prime) > eps:
         al = al_prime
         print('Computing new weights...')
-        mu = beta * _derivatives(degree, base_kernels, mu, al) ##
+        v = _derivatives(base_kernels, mu, al) ##
+		v /= np.linalg.norm(v)		
+		mu = mu0 + L * v 
         print('Computing new alpha...')
-		gram = sum_weigh_ker(base_kernels, mu) ** degree + lam * np.eye(m)
+		gram = sum_weigh_ker(base_kernels, mu) + lam * np.eye(m)
         al_prime = eta * al + (1. - eta) * np.linalg.solve(gram,labels)
         print('The weight vector for this round is:\n', mu)
-    poly_ker = sum_weight_ker(base_kernels, mu) ** degree
-    return mu, poly_ker
+    return sum_weight_ker(base_kernels, mu)
 
 def _derivatives(degree, base_kernels, mu, al):
     derivatives = []
 	sum_ker = sum_weight_kernels(base_kernels, mu)
     for k in range(p):
-        center = (sum_ker ** (degree -1)) * base_kernels[k]
-        derivatives.append(degree * ((al.T).dot(center)).dot(al))
+        derivatives.append(((al.T).dot(sum_ker)).dot(al))
     return np.array(derivatives)
 
 def sum_weight_kernels(base_kernels, mu):
